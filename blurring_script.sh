49826ce787f8ca7f85863369c77296dae31945b0
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2021-11-12 16:33:43 +0100

    Update blurring_script.sh

--- blurring_script.sh
+++ blurring_script.sh
@@ -1,15 +1,10 @@
-# awk -v v1=$var1 -v v2=$var2 'BEGIN {print 141*(v1*0.0113/0.9)^-1.209*0.993^v2}' cre >> ggh
+e=$(bc -l <<< "e($1*l(10))")
 
-x=$(awk -v v1=$1 'BEGIN {print 1-2*v1}')
+x=$(awk -v v1=$e 'BEGIN {print 1-2*v1}')
 
 y=$(awk -v v1=$x 'BEGIN {print log(v1)/log(10)}')
-loge=$(awk -v v1=$1 'BEGIN {print log(v1)/log(10)}')
+#loge=$(awk -v v1=$1 'BEGIN {print log(v1)/log(10)}')
 
-#y=$(echo 'l($x)/l(10)' | bc -l)
-#loge=$(echo 'l($1)/l(10)' | bc -l)
 
-#echo $y
-#echo $loge
-
-sed -e 's/\tGT/\tGL/g' -e 's/0|0/'$y','$loge','$loge'/g' -e 's/0|1/'$loge','$y','$loge'/g' -e 's/1|0/'$loge','$y','$loge'/g' \
--e 's/1|1/'$loge','$loge','$y'/g' $2
+sed -e 's/\tGT/\tGL/g' -e 's/0|0/'$y','$1','$1'/g' -e 's/0|1/'$1','$y','$1'/g' -e 's/1|0$
+-e 's/1|1/'$1','$1','$y'/g' $2
