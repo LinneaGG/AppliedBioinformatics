4816d8c46990391c7ed943628ac14fbc4ab5fe18
LinneaGG <78213575+LinneaGG@users.noreply.github.com>
2021-11-15 10:58:17 +0100

    Update blurring_script.sh

--- blurring_script.sh
+++ blurring_script.sh
@@ -6,5 +6,4 @@ y=$(awk -v v1=$x 'BEGIN {print log(v1)/log(10)}')
 #loge=$(awk -v v1=$1 'BEGIN {print log(v1)/log(10)}')
 
 
-sed -e 's/\tGT/\tGL/g' -e 's/0|0/'$y','$1','$1'/g' -e 's/0|1/'$1','$y','$1'/g' -e 's/1|0$
--e 's/1|1/'$1','$1','$y'/g' $2
+sed -e 's/\tGT/\tGL/g' -e 's/0|0/'$y','$1','$1'/g' -e 's/0|1/'$1','$y','$1'/g' -e 's/1|0/'$1','$y','$1'/g' -e 's/1|1/'$1','$1','$y'/g' $2
