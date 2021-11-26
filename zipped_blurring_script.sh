1b0d512db7507a550bc3fd027fa9689875c1063e
albinlundin <78469232+albinlundin@users.noreply.github.com>
2021-11-26 10:00:12 +0100

    Update zipped_blurring_script.sh

--- zipped_blurring_script.sh
+++ zipped_blurring_script.sh
@@ -6,4 +6,4 @@ y=$(awk -v v1=$x 'BEGIN {print log(v1)/log(10)}')
 #loge=$(awk -v v1=$1 'BEGIN {print log(v1)/log(10)}')
 
 
-zless $2 | sed -e 's/\tGT/\tGL/g' -e 's/0|0/'$y','$1','$1'/g' -e 's/0|1/'$1','$y','$1'/g' -e 's/1|0/'$1','$y','$1'/g' -e 's/1|1/'$1','$1','$y'/g'
+zless $2 | sed -e 's/\tGT/\tGL/g' -e 's/0|0/'$y','$1','$1'/g' -e 's/0|1/'$1','$y','$1'/g' -e 's/1|0/'$1','$y','$1'/g' -e 's/1|1/'$1','$1','$y'/g' -e 's/##FORMAT=<ID=GT/##FORMAT=<ID=GL/g'
